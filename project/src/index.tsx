80550ffa04b3af1c15f3cba105b864af84babbf8
Azarak <abbatd@gmail.com>
2021-11-02 01:10:27 +1000

    Добавил проверку авторизации

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,18 +8,25 @@ import {
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
-import { fetchFilmsAction } from './store/api-actions';
+import {
+  checkAuthAction,
+  fetchFilmsAction
+} from './store/api-actions';
 import { reducer } from './store/reducer';
+import { requireAuthorization } from './store/action';
 import { createAPI } from './services/api';
 import { AuthorizationStatus } from './const';
 import type { ThunkAppDispatch } from './types/action';
 
-const api = createAPI(() => AuthorizationStatus.Auth);
+const api = createAPI(
+  () => store.dispatch(requireAuthorization(AuthorizationStatus.NoAuth)),
+);
 
 const store = createStore(reducer, composeWithDevTools(
   applyMiddleware(thunk.withExtraArgument(api)),
 ));
 
+(store.dispatch as ThunkAppDispatch)(checkAuthAction());
 (store.dispatch as ThunkAppDispatch)(fetchFilmsAction());
 
 ReactDOM.render(
