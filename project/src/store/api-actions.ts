cb4c2a2a7b8cea3e726e1a767a74a3c5f7a02b4b
Azarak <abbatd@gmail.com>
2021-11-01 23:15:22 +1000

    Добавил асинхронные действия login и logout

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,10 +1,45 @@
-import { loadFilms } from './action';
-import { APIRoute } from '../const';
-import type { ThunkActionResult } from '../types/action';
+import {
+  loadFilms,
+  requireAuthorization,
+  requireLogout
+} from './action';
+import {
+  dropToken,
+  saveToken,
+  Token
+} from '../services/token';
+import {
+  APIRoute,
+  AuthorizationStatus
+} from '../const';
+import type { AuthData } from '../types/auth-data';
 import type { FilmFromServer } from '../types/film';
+import type { ThunkActionResult } from '../types/action';
 
 export const fetchFilmsAction = (): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
     const {data} = await api.get<FilmFromServer[]>(APIRoute.Films);
     dispatch(loadFilms(data));
+  };
+
+export const checkAuthAction = (): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    await api.get(APIRoute.Login)
+      .then(() => {
+        dispatch(requireAuthorization(AuthorizationStatus.Auth));
+      });
+  };
+
+export const loginAction = ({login: email, password}: AuthData): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    const {data: {token}} = await api.post<{token: Token}>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  };
+
+export const logoutAction = (): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireLogout());
   };
