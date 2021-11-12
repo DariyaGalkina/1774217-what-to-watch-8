f4dbdfbc1ffcf28d8489c8d18be182b2fcd331cf
Azarak <abbatd@gmail.com>
2021-11-12 23:39:21 +1000

    Рефактор reducer и actions

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,3 +1,4 @@
+import { createAction } from '@reduxjs/toolkit';
 import { AuthorizationStatus } from '../const';
 import { ActionType } from '../types/action';
 import type {
@@ -6,46 +7,88 @@ import type {
 } from '../types/film';
 import type { ReviewProps } from '../types/review';
 
-export const changeGenre = (genre: string) => ({
-  type: ActionType.ChangeGenre,
-  payload: genre,
-} as const);
+// export const changeGenre = (genre: string) => ({
+//   type: ActionType.ChangeGenre,
+//   payload: genre,
+// } as const);
 
-export const filterFilms = (films: FilmProps[]) => ({
-  type: ActionType.FilterFilms,
-  payload: films,
-} as const);
+export const changeGenre = createAction(
+  ActionType.ChangeGenre,
+  (genre: string) => ({payload: genre}),
+);
 
-export const loadFilms = (films: FilmFromServer[]) => ({
-  type: ActionType.LoadFilms,
-  payload: films,
-} as const);
+// export const filterFilms = (films: FilmProps[]) => ({
+//   type: ActionType.FilterFilms,
+//   payload: films,
+// } as const);
 
-export const loadFilm = (film: FilmFromServer) => ({
-  type: ActionType.LoadFilm,
-  payload: film,
-} as const);
+export const filterFilms = createAction(
+  ActionType.FilterFilms,
+  (films: FilmProps[]) => ({payload: films}),
+);
 
-export const loadSimilarFilms = (films: FilmFromServer[]) => ({
-  type: ActionType.LoadSimilarFilms,
-  payload: films,
-} as const);
+// export const loadFilms = (films: FilmFromServer[]) => ({
+//   type: ActionType.LoadFilms,
+//   payload: films,
+// } as const);
 
-export const loadReviews = (reviews: ReviewProps[]) => ({
-  type: ActionType.LoadReviews,
-  payload: reviews,
-} as const);
+export const loadFilms = createAction(
+  ActionType.LoadFilms,
+  (films: FilmFromServer[]) => ({payload: films}),
+);
 
-export const requireAuthorization = (authStatus: AuthorizationStatus) => ({
-  type: ActionType.RequireAuthorization,
-  payload: authStatus,
-} as const);
+// export const loadFilm = (film: FilmFromServer) => ({
+//   type: ActionType.LoadFilm,
+//   payload: film,
+// } as const);
 
-export const requireLogout = () => ({
-  type: ActionType.RequireLogout,
-} as const);
+export const loadFilm = createAction(
+  ActionType.LoadFilm,
+  (film: FilmFromServer) => ({payload: film}),
+);
 
-export const redirectToRoute = (url: string) => ({
-  type: ActionType.RedirectToRoute,
-  payload: url,
-} as const);
+// export const loadSimilarFilms = (films: FilmFromServer[]) => ({
+//   type: ActionType.LoadSimilarFilms,
+//   payload: films,
+// } as const);
+
+export const loadSimilarFilms = createAction(
+  ActionType.LoadSimilarFilms,
+  (films: FilmFromServer[]) => ({payload: films}),
+);
+
+// export const loadReviews = (reviews: ReviewProps[]) => ({
+//   type: ActionType.LoadReviews,
+//   payload: reviews,
+// } as const);
+
+export const loadReviews = createAction(
+  ActionType.LoadReviews,
+  (reviews: ReviewProps[]) => ({payload: reviews}),
+);
+
+// export const requireAuthorization = (authStatus: AuthorizationStatus) => ({
+//   type: ActionType.RequireAuthorization,
+//   payload: authStatus,
+// } as const);
+
+export const requireAuthorization = createAction(
+  ActionType.RequireAuthorization,
+  (authStatus: AuthorizationStatus) => ({payload: authStatus}),
+);
+
+// export const requireLogout = () => ({
+//   type: ActionType.RequireLogout,
+// } as const);
+
+export const requireLogout = createAction(ActionType.RequireLogout);
+
+// export const redirectToRoute = (url: string) => ({
+//   type: ActionType.RedirectToRoute,
+//   payload: url,
+// } as const);
+
+export const redirectToRoute = createAction(
+  ActionType.RedirectToRoute,
+  (url: string) => ({payload: url}),
+);
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,18 +1,26 @@
+import { createReducer } from '@reduxjs/toolkit';
 import {
   adaptFilmsToClient,
   adaptToClient,
   filterFilmsByGenre
 } from '../utils';
 import {
+  changeGenre,
+  filterFilms,
+  loadFilm,
+  loadFilms,
+  loadReviews,
+  loadSimilarFilms,
+  requireAuthorization,
+  requireLogout
+} from './action';
+import {
   AuthorizationStatus,
   Genres
 } from '../const';
-import {
-  Actions,
-  ActionType
-} from '../types/action';
+// import { ActionType } from '../types/action';
 import type { State } from '../types/state';
-import { FilmProps } from '../types/film';
+import type { FilmProps } from '../types/film';
 
 const initialState: State = {
   currentGenre: Genres.All,
@@ -27,46 +35,82 @@ const initialState: State = {
   authorizationStatus: AuthorizationStatus.Unknown,
 };
 
-export const reducer = (state: State = initialState, action: Actions): State => {
-  switch (action.type) {
-    case ActionType.ChangeGenre:
-      return {...state, currentGenre: action.payload};
-    case ActionType.FilterFilms:
-      return {...state, filteredFilms: filterFilmsByGenre(action.payload, state.currentGenre)};
-    case ActionType.LoadFilms: {
+export const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(changeGenre, (state, action) => {
+      state.currentGenre = action.payload;
+    })
+    .addCase(filterFilms, (state, action) => {
+      state.filteredFilms = filterFilmsByGenre(action.payload, state.currentGenre);
+    })
+    .addCase(loadFilms, (state, action) => {
       const adaptedFilms = adaptFilmsToClient(action.payload);
 
-      return {
-        ...state,
-        filmList: adaptedFilms,
-        filteredFilms: adaptedFilms,
-        isDataLoaded: true,
-      };
-    }
-    case ActionType.LoadFilm:
-      return {
-        ...state,
-        currentFilm: adaptToClient(action.payload),
-        isSimilarFilmsLoaded: false,
-        isReviewsLoaded: false,
-      };
-    case ActionType.LoadSimilarFilms:
-      return {
-        ...state,
-        similarFilms: adaptFilmsToClient(action.payload),
-        isSimilarFilmsLoaded: true,
-      };
-    case ActionType.LoadReviews:
-      return {
-        ...state,
-        reviews: action.payload,
-        isReviewsLoaded: true,
-      };
-    case ActionType.RequireAuthorization:
-      return {...state, authorizationStatus: action.payload};
-    case ActionType.RequireLogout:
-      return {...state, authorizationStatus: AuthorizationStatus.NoAuth};
-    default:
-      return state;
-  }
-};
+      state.filmList = adaptedFilms;
+      state.filteredFilms = adaptedFilms;
+      state.isDataLoaded = true;
+    })
+    .addCase(loadFilm, (state, action) => {
+      state.currentFilm = adaptToClient(action.payload);
+      state.isSimilarFilmsLoaded = false;
+      state.isReviewsLoaded = false;
+    })
+    .addCase(loadSimilarFilms, (state, action) => {
+      state.similarFilms = adaptFilmsToClient(action.payload);
+      state.isSimilarFilmsLoaded = true;
+    })
+    .addCase(loadReviews, (state, action) => {
+      state.reviews = action.payload;
+      state.isReviewsLoaded = true;
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
+    })
+    .addCase(requireLogout, (state) => {
+      state.authorizationStatus = AuthorizationStatus.NoAuth;
+    });
+});
+
+// export const reducer = (state: State = initialState, action: Actions): State => {
+//   switch (action.type) {
+//     case ActionType.ChangeGenre:
+//       return {...state, currentGenre: action.payload};
+//     case ActionType.FilterFilms:
+//       return {...state, filteredFilms: filterFilmsByGenre(action.payload, state.currentGenre)};
+//     case ActionType.LoadFilms: {
+//       const adaptedFilms = adaptFilmsToClient(action.payload);
+
+//       return {
+//         ...state,
+//         filmList: adaptedFilms,
+//         filteredFilms: adaptedFilms,
+//         isDataLoaded: true,
+//       };
+//     }
+//     case ActionType.LoadFilm:
+//       return {
+//         ...state,
+//         currentFilm: adaptToClient(action.payload),
+//         isSimilarFilmsLoaded: false,
+//         isReviewsLoaded: false,
+//       };
+//     case ActionType.LoadSimilarFilms:
+//       return {
+//         ...state,
+//         similarFilms: adaptFilmsToClient(action.payload),
+//         isSimilarFilmsLoaded: true,
+//       };
+//     case ActionType.LoadReviews:
+//       return {
+//         ...state,
+//         reviews: action.payload,
+//         isReviewsLoaded: true,
+//       };
+//     case ActionType.RequireAuthorization:
+//       return {...state, authorizationStatus: action.payload};
+//     case ActionType.RequireLogout:
+//       return {...state, authorizationStatus: AuthorizationStatus.NoAuth};
+//     default:
+//       return state;
+//   }
+// };
